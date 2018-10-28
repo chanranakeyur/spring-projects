0f4362fee108c8fb0dee468c827d6b1555368657
keyur <keyur@keyur-PC>
2018-10-28 19:24:54 +0530

    28-10-2018_7:24

--- IOC_Test/src/beans/Test.java
+++ IOC_Test/src/beans/Test.java
@@ -1,5 +1,9 @@
 package beans;
 
 public class Test {
-
+	
+	public Test() {
+		System.out.println("Test...");
+	}
+	
 }
---
+++ IOC_Test/src/test/Client.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@SuppressWarnings("deprecation")
+public class Client {
+	public static void main(String[] args) {
+		Resource resource=new ClassPathResource("resources/spring.xml");
+		
+		@SuppressWarnings("deprecation")
+		BeanFactory factory=new XmlBeanFactory(resource);
+		
+		System.out.println("Document Valid");
+	}
+}
