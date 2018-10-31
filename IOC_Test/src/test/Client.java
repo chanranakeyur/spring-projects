8ae87efb2ce122855824b363d1ae713afbd2c217
keyur <keyur@keyur-PC>
2018-10-31 11:52:57 +0530

    31-10-2018_11:51AM

--- IOC_Test/src/test/Client.java
+++ IOC_Test/src/test/Client.java
@@ -14,6 +14,8 @@ public class Client {
 		BeanFactory factory=new XmlBeanFactory(resource);
 		
 		factory.getBean("t");
+		factory.getBean("t");
+		factory.getBean("t");
 		System.out.println("Document Valid");
 	}
 }
