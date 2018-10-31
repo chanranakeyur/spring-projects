438b67a75054a91c34193db8b5a3c8e19048298c
keyur <keyur@keyur-PC>
2018-10-31 18:00:09 +0530

    31-10-2018/6:00PM

---
+++ Priveate Constructor Access/src/test/Client.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.lang.reflect.Constructor;
+
+public class Client
+{
+	public static void main(String args[]) 
+	{
+		try
+		{
+			Class c=Class.forName("beans.Test");
+			Constructor con[]=c.getDeclaredConstructors();
+						con[0].setAccessible(true);
+						con[0].newInstance();
+		}
+		catch (Exception e) 
+		{
+			
+		}
+	}
+}
